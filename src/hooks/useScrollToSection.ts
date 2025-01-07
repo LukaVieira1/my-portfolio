f73afd5a5a28543c4ac604c3ada7b1a79bd09c18
LukaVieira1 <lukavieira14@gmail.com>
2025-01-07 14:11:30 -0300

    add: use Scroll hook

---
+++ src/hooks/useScrollToSection.ts
@@ -0,0 +1,17 @@
+export function useScrollToSection() {
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const navHeight = 80;
+      const elementPosition = element.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - navHeight;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return scrollToSection;
+}
