4665ed62c501e3fe82b43e767829cbf598b2ccc0
LukaVieira1 <lukavieira14@gmail.com>
2025-01-08 11:36:49 -0300

    add: theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#64ffda",
+          DEFAULT: "#64ffda",
+          dark: "#64ffda20",
+        },
+        background: {
+          DEFAULT: "#0a192f",
+          light: "#112240",
+          lighter: "#233554",
+        },
+        text: {
+          primary: "#ccd6f6",
+          secondary: "#8892b0",
+        },
+      },
+      maxWidth: {
+        container: "1024px",
+      },
+      height: {
+        section: "600px",
+      },
+    },
   },
   plugins: [],
 };
